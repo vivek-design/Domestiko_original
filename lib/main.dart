5f4f3d77758ee5b8a772a9713577b2dd53d7d87d
vivek chouhan <79709538+vivek-design@users.noreply.github.com>
2024-03-17 11:22:54 +0530

    no too much

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: router.homepage,
 
+
       routes: {
         router.registerr2: (context) => registermaid(),
         router.Setting: (context) => setting_page(),
